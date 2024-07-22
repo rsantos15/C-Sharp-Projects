dbdd4258d381ae9b0de353bbb1ef1f1d20236f31
rsantos15 <mafodofu@gmail.com>
2024-07-22 14:04:50 -0600

    Accounting, Hello World and Program
    Simple scripts for hello world, accounting and compiling program to exe.

---
+++ Basic_C#_Programs/Hello World/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Fuck my life, World!");
+        Console.ReadLine();
+
+        }  
+}
+
---
+++ Basic_C#_Programs/myConsoleProject.cs
@@ -0,0 +1,10 @@
+﻿
+using System
+
+static void Main()
+{ 
+    Console.WriteLine("What is your name?");
+    string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name + "!");
+
+}
