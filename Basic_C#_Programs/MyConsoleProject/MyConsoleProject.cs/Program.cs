77cc7246163b5d8707416d96d5927913b451a2b7
rsantos15 <mafodofu@gmail.com>
2024-07-22 12:23:34 -0600

    Hello World
    Just a hello world sample

---
+++ Basic_C#_Programs/MyConsoleProject/MyConsoleProject.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyConsoleProject.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+            Console.Writeline("What is your name?");
+            string name = Console.Readline();
+        MyConsoleProject.WriteLine("Hello, " + Ella +  "!");
+            Console.Read();
+            }
+}
+    }
+}
---
+++ Basic_C#_Programs/SampleApplication.cs
@@ -0,0 +1,6 @@
+int num1 = 7;
+int num2 = 12;
+
+string result =  num1 > num2 ? "numn1 is greater than num2" : "num1 is nt greater than num2";
+Console.WriteLine(result);
+
